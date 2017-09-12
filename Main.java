603d73bfd4e89248ac765b9736f3ab91172a15e0
CoderBose <priyankabose22812@gmail.com>
2017-09-12 12:36:21 +0530

    Add Edit

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		Student s = new Student;
+		s.id = 1;
+		s.fullName = "ABC XYZ";
+		s.birthDate = "1996-04-26";
+		s.avgMark = 90.25;
+		add(s,1);
+	}
+
+}
